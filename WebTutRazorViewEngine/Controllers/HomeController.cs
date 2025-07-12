d3b5043805edc74639ae246c22a8e208d6828cc3
Richmond Batoto <richmondbatoto@gmail.com>
2025-07-12 23:21:43 +0800

    add WebTutRazorViewEngine

---
+++ WebTutRazorViewEngine/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebTutRazorViewEngine.Models;
+
+namespace WebTutRazorViewEngine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var person = new PersonViewModel()
+            {
+                FirstName = "Richmond",
+                LastName = "Batoto",
+                Gender = Gender.Male,
+                Age = 21,
+                FavoriteMovies = new List<string> { "Pirates of the Caribbean", "Miss Peregrine Home for Peculiar Children" }
+            };
+
+            return View(person);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new PersonViewModel());
+        }
+
+        public IActionResult Createv2()
+        {
+            return View(new PersonViewModel()
+            {
+                FirstName = "Frace",
+                IsVoter = true,
+                Profession = "Programmer"
+            });
+        }
+
+        public IActionResult Save(PersonViewModel submittedMode) 
+        {
+            //Do something here
+
+            return View("Result", submittedMode);
+        }
+
+        public IActionResult TestFullName()
+        {
+            return PartialView("_FullName", new PersonViewModel
+            {
+                FirstName = "Test",
+                LastName = "Only"
+            });
+        }
+    }
+}
---
+++ WebTutRazorViewEngine/Models/PersonViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebTutRazorViewEngine.Models
+{
+    public class PersonViewModel
+    {
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public int Age {  get; set; }
+        public Gender Gender { get; set; }
+        public List<string> FavoriteMovies { get; set; }
+        public bool IsVoter {  get; set; }
+        public string Profession { get; set; }
+        public AddressViewModel Address { get; set; }
+    }
+
+    public class AddressViewModel
+    {
+        public string Brgy { get; set; }
+        public string City { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
